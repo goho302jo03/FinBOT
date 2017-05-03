16737aed2545d170749047451ade4bd7044de935
johnson <johnson44513@gmail.com>
2017-05-03 22:35:27 +0800

    yeah

--- finbot.js
+++ finbot.js
@@ -5,19 +5,15 @@ function say(){
 
 function append(line){
   var dialogbox = document.getElementById('dialogbox');
-  dialogbox.innerHTML+=line+"<br/>\n;
+  dialogbox.innerHTML+=line+"<br/>\n";
   dialogbox.scrollTop = dialogbox.scrollHeight;
 }
 
-$(document).ready(function{
+$(document).ready(function(){
 
   $('#fun').on('click',function(){
     $('#funul').toggle('slow');
-  }  
-
-  $('#send').click(say(){
-    $('.middle').append(
-  }
+  }); 
 
 
 });
