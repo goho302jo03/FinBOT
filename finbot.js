8964f2d8bd2661ad66301dd01a63d5b988344c49
johnson <johnson44513@gmail.com>
2017-05-04 01:29:10 +0800

    too hard

--- finbot.js
+++ finbot.js
@@ -1,21 +1,26 @@
 
-function say(){
-  append(document.getElementById('comment').value);
-}
-
-function append(line){
-  var dialogbox = document.getElementById('dialogbox');
-  dialogbox.innerHTML+=line+"<br/>\n";
-  dialogbox.scrollTop = dialogbox.scrollHeight;
-}
 
 $(document).ready(function(){
+  var comment = null;
+
+  function say(){
+    append(document.getElementById('comment').value);
+  }
+
+  function append(line){
+    var dialogbox = document.getElementById('dialogbox');
+    dialogbox.innerHTML+=line+"<br/>\n";
+    dialogbox.scrollTop = dialogbox.scrollHeight;
+  }
 
   $('#fun').on('click',function(){
     $('#funul').toggle('slow');
   }); 
 
-
+  $('#send').on('click',function(){
+    comment = document.getElementById('comment')
+    document.write(comment+'<br/>');
+  });
 });
 
 
