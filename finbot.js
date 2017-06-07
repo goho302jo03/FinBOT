0c39453d1dddaddd5ead731c8f761f702ef5e790
johnson <johnson44513@gmail.com>
2017-06-07 16:53:25 +0800

    gold chart setting

--- finbot.js
+++ finbot.js
@@ -327,13 +327,12 @@ $(document).ready(function(){
       else if(funcNum==6){
          funcNum = 0;
 
-          $('.dialogbox').append(
-            "<div class=\"finbot\">"+
-            "<a><img class=\"finpic\" src=\"./images/chatroom/finpic.png\"></a>"+
-            "<div class=\"frecord\"> 請稍等喔 </div>");
+          var goldChart;
+          
+          goldChart+=1;
           
           $.get("./goldprice.njs",$("#comment").val(),
-        
+          
           function(data){     
             $('.dialogbox').append(
               "<div class=\"finbot\">"+
@@ -349,7 +348,6 @@ $(document).ready(function(){
             
               var goldprice = data.everydayGoldpriceUSD;
               var date = data.date;
-                  goldChart+=1;
               var ctx = document.getElementsByClassName(goldChart);
               var goldchart = new Chart(ctx,{
                   type: 'line',
@@ -368,6 +366,11 @@ $(document).ready(function(){
                   }   
                   });   
            },"json");
+
+          $('.dialogbox').append(
+            "<div class=\"finbot\">"+
+            "<a><img class=\"finpic\" src=\"./images/chatroom/finpic.png\"></a>"+
+            "<div class=\"frecord\"> 請稍等喔 </div>");
        }   
       $('#comment').val("");
     }
